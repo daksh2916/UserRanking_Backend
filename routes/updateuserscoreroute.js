43d78ce7824ada139ace32d2784b397da430f6bf
Daksh Tiwari <dakshtiwari2602@gmail.com>
2024-02-10 13:31:03 +0530

    updates

--- routes/updateuserscoreroute.js
+++ routes/updateuserscoreroute.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const userScoreController = require('../controllers/updateuserscorecontroller'); // Corrected import path
-const UserController = require('../controllers/checkusercontroller');
+//const UserController = require('../controllers/checkusercontroller');
 
 // Route to check if a user exists with the given userId
-router.get('/api/v1/user/:userId', UserController.checkUserExists);
+//router.get('/api/v1/user/:userId', UserController.checkUserExists);
 
 
 // Route for updating user score
--- routes/usercreate.js
+++ routes/usercreate.js
@@ -2,10 +2,10 @@ const express=require("express");
 const router=express.Router();
 
 const {createuserscore}=require("../controllers/createuserscorecontroller");
-const UserController = require('../controllers/checkusercontroller');
+//const UserController = require('../controllers/checkusercontroller');
 
 // Route to check if a user exists with the given userId
-router.get('/api/v1/user/:userId', UserController.checkUserExists);
+//router.get('/api/v1/user/:userId', UserController.checkUserExists);
 
 
 router.post("/createuserscore",createuserscore);
